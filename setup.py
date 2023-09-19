347efb44f7adcebbe0d5ff15b1ad4a15f08052ec
CherithaK <cherrycheritha3@gmail.com>
2023-09-19 13:58:08 +0530

    setup.py, requirements.txt, src folder and packages.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    ''' 
+    This funtion will return the list of requirements 
+    '''
+    requirements=[]
+    with open('requirements.txt') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='cherry',
+author_email='cherrycheritha3@gmail.com',
+packages = find_packages(),
+install_requires=get_requirements('requirement.txt')
+
+)
